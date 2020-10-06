6c2aaad9a138850c8596e7c26dacf811d91cb2b6
Arif <arifkhoiruman2@gmail.com>
2020-10-06 21:57:01 +0700

    DAY7

--- app/src/main/java/com/example/dts_day7/Home.java
+++ app/src/main/java/com/example/dts_day7/Home.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 
 public class Home extends AppCompatActivity {
     Button btnInput, btnKal, btnList;
@@ -23,7 +22,7 @@ public class Home extends AppCompatActivity {
         btnInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Home.this, MainActivity.class);
+                Intent intent = new Intent(Home.this, inputActivity.class);
                 startActivity(intent);
             }
         });
